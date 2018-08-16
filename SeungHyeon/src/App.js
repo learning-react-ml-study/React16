e1ed99b5932f65791462cf1e20d9f53cbc28b0ad
crobbitlucy <crobbitlucy@gmail.com>
2018-08-16 15:19:55 +0900

    SeungHyeon #5 Error Boundaries 완강 - update App.js note.md

--- SeungHyeon/src/App.js
+++ SeungHyeon/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import { createPortal } from 'react-dom';
 
+// ## return types exercise
+
 // 1. 여러가지 엘리먼트 return 가능!
 
 //원래는 여러개의 엘리먼트를 리턴하려면 하나의 컴포넌트에 감싸주어야 했지만(ex. array+key 부여 or span에 싸기) 이제 바로 리턴 가능.
@@ -41,6 +43,8 @@ class StringReturn extends Component{
   }
 }
 
+// ## Portals exercise
+
 //엘리먼트가 아닌 포털을 리턴함
 // 포털은 리액트 안에 (X) ReactDOM 안에 (O)
 class Portals extends Component{
@@ -54,19 +58,48 @@ class Portals extends Component{
 }
 const Message = () => "Just touched it!";
 
+
+// ## Error Boundaries exercise
+
+class ErrorMaker extends Component{
+  state = {
+    friends: ["jisu", "flynn", "daal", "kneeprayer"]
+  };
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      });
+    }, 2000);
+  };
+  render(){
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend} `);
+  }
+}
+
+const ErrorFallback = () => "Sorry something went wrong";
+
 class App extends Component {
+  state = {
+    hasError: false
+  };
+  componentDidCatch = (error, info) => {
+    // console.log(`catched ${error} the info i have is ${JSON.stringify(info)}`);
+    this.setState({
+        hasError: true
+      });
+  };
   render() {
+    const { hasError } = this.state;
     return (
       <Fragment>
         <StringReturn />
         <Portals />
+        {hasError ? <ErrorFallback /> : <ErrorMaker />}
       </Fragment>
     );
   }
 }
-
-
-
-
 
 export default App;

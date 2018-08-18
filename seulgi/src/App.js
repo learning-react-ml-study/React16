c011de0d4131f86f634e992a7532e6b6820ad212
seultt <seultt04@gmail.com>
2018-08-18 12:07:49 +0900

    #5 Error Boundaries with Higher Order Components

--- seulgi/src/App.js
+++ seulgi/src/App.js
@@ -1,6 +1,27 @@
 import React, { Component, Fragment } from 'react'
 import { createPortal } from 'react-dom'
-import { stringify } from 'querystring'
+
+const BoundaryHOC = ProtectedComponent =>
+  class BoundaryHOC extends Component {
+    state = {
+      hasError: false
+    }
+
+    componentDidCatch = () => {
+      this.setState({
+        hasError: true
+      })
+    }
+
+    render() {
+      const { hasError } = this.state
+      if (hasError) {
+        return <ErrorFallback />
+      } else {
+        return <ProtectedComponent />
+      }
+    }
+  }
 
 class ErrorMaker extends Component {
   state = {
@@ -13,17 +34,22 @@ class ErrorMaker extends Component {
       })
     }, 3)
   }
+
   render() {
     const { friends } = this.state
     return friends.map(friend => ` ${friend} `)
   }
 }
 
+const PErrorMaker = BoundaryHOC(ErrorMaker)
+
 class Portals extends Component {
   render() {
     return createPortal(<Message />, document.getElementById('touchme'))
   }
 }
+
+const PPortals = BoundaryHOC(Portals)
 
 const Message = () => 'Just touch it!'
 
@@ -36,25 +62,16 @@ class ReturnTypes extends Component {
 const ErrorFallback = () => 'Sorry something went wrong'
 
 class App extends Component {
-  state = {
-    hasError: false
-  }
-  componentDidCatch = (error, info) => {
-    console.log(`catched ${error} the info i have is ${JSON.stringify(info)}`)
-    this.setState({
-      hasError: true
-    })
-  }
   render() {
     const { hasError } = this.state
     return (
       <Fragment>
         <ReturnTypes />
-        <Portals />
-        {hasError ? <ErrorFallback /> : <ErrorMaker />}
+        <PPortals />
+        <PErrorMaker />
       </Fragment>
     )
   }
 }
 
-export default App
+export default BoundaryHOC(App)
